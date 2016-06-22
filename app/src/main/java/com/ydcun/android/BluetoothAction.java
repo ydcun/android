a4b7ff1551e5229a500cd05c2be77bd0f7f1b30d
ydcun <ydcun@Hotmail.com>
2016-06-22 16:01:38 +0800

    update

---
+++ app/src/main/java/com/ydcun/android/BluetoothAction.java
@@ -0,0 +1,16 @@
+package com.ydcun.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ydcun_home on 2016/6/22.
+ */
+public class BluetoothAction extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bluetooth);
+    }
+}
--- app/src/main/java/com/ydcun/android/MainActivity.java
+++ app/src/main/java/com/ydcun/android/MainActivity.java
@@ -1,9 +1,11 @@
 package com.ydcun.android;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
 public class MainActivity extends Activity {
@@ -12,6 +14,9 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void openBluetoothActivity(View v){
+        startActivity(new Intent(this, BluetoothAction.class));
     }
 
     @Override
